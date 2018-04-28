19e34d3418ecccd9ac3edc3029a8dcf6f399c6e9
nsimsiri <nsimsiri@umass.edu>
2018-04-27 23:06:35 -0400

    experiment1

--- train.py
+++ train.py
@@ -97,7 +97,8 @@ def main(args):
                       %(epoch, args.num_epochs, i, total_step,
                         loss.data[0], np.exp(loss.data[0]), time.time()-t0))
             else:
-                print '%s - loss: %.4f - time: %.4f'%(i, loss.data[0], time.time()-t0);
+                pass
+                # print '%s - loss: %.4f - time: %.4f'%(i, loss.data[0], time.time()-t0);
 
             # Save the models
             # if (i+1) % args.save_step == 0:
@@ -110,6 +111,7 @@ def main(args):
             #                             name_pretrained_sizes(args.embed_size, args.hidden_size, "encoder")),
             #                             'encoder-%d-%d.pkl' %(epoch+1, i+1)))
     print 'saving final model';
+    print '%s - loss: %.4f - time: %.4f'%(i, loss.data[0], time.time()-t0);
     torch.save(decoder.state_dict(),
                os.path.join(args.model_path,
                             name_pretrained_sizes(args.num_layers, args.embed_size, args.hidden_size, "decoder", epoch, i)))
@@ -145,7 +147,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_layers', type=int , default=1 ,
                         help='number of layers in lstm')
 
-    parser.add_argument('--num_epochs', type=int, default=1)
+    parser.add_argument('--num_epochs', type=int, default=5)
     parser.add_argument('--batch_size', type=int, default=25) #128
     parser.add_argument('--num_workers', type=int, default=2)
     parser.add_argument('--learning_rate', type=float, default=0.005)
