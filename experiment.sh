6da7184a6f29427a462b11780533004f18ce5abd
nsimsiri <nsimsiri@umass.edu>
2018-04-28 11:35:09 -0400

    hi

--- experiment.sh
+++ experiment.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
-python train.py --embed_size=64 --hidden_size=128 --num_layers=1
-python train.py --embed_size=64 --hidden_size=256 --num_layers=1
-python train.py --embed_size=64 --hidden_size=512 --num_layers=1
-
-python train.py --embed_size=128 --hidden_size=128 --num_layers=1
-python train.py --embed_size=128 --hidden_size=256 --num_layers=1
-python train.py --embed_size=128 --hidden_size=512 --num_layers=1
-
-python train.py --embed_size=256 --hidden_size=128 --num_layers=1
-python train.py --embed_size=256 --hidden_size=256 --num_layers=1
-python train.py --embed_size=256 --hidden_size=512 --num_layers=1
-
-python train.py --embed_size=64 --hidden_size=128 --num_layers=2
-python train.py --embed_size=64 --hidden_size=256 --num_layers=2
-python train.py --embed_size=64 --hidden_size=512 --num_layers=2
-
-python train.py --embed_size=128 --hidden_size=128 --num_layers=2
+# python train.py --embed_size=64 --hidden_size=128 --num_layers=1
+# python train.py --embed_size=64 --hidden_size=256 --num_layers=1
+# python train.py --embed_size=64 --hidden_size=512 --num_layers=1
+#
+# python train.py --embed_size=128 --hidden_size=128 --num_layers=1
+# python train.py --embed_size=128 --hidden_size=256 --num_layers=1
+# python train.py --embed_size=128 --hidden_size=512 --num_layers=1
+#
+# python train.py --embed_size=256 --hidden_size=128 --num_layers=1
+# python train.py --embed_size=256 --hidden_size=256 --num_layers=1
+# python train.py --embed_size=256 --hidden_size=512 --num_layers=1
+#
+# python train.py --embed_size=64 --hidden_size=128 --num_layers=2
+# python train.py --embed_size=64 --hidden_size=256 --num_layers=2
+# python train.py --embed_size=64 --hidden_size=512 --num_layers=2
+#
+# python train.py --embed_size=128 --hidden_size=128 --num_layers=2
 python train.py --embed_size=128 --hidden_size=256 --num_layers=2
 python train.py --embed_size=128 --hidden_size=512 --num_layers=2
 
