fa709296cb6e579eab3c2197bfef20aed875089b
nsimsiri <nsimsiri@umass.edu>
2018-04-28 16:17:09 -0400

    debugging attention with cuda error

--- att_model.py
+++ att_model.py
@@ -61,7 +61,7 @@ class DecoderRNN(nn.Module):
         print 'captions', captions
         embeddings = self.embed(captions)
         # embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
-        next_c = Variable(torch.zeros(N, self.hidden_size)) #need cuda
+        next_c = Variable(torch.zeros(N, self.hidden_size)).cuda() #need cuda
         # next_h = Variable(torch.zeros(N, self.hidden_size))
         next_h = self.affine_lstm_init(features);
         print 'embeddings', embeddings;
