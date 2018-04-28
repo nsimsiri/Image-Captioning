0f978064bc1b3e0c9fc4fbc1e20191a0021a87ec
nsimsiri <nsimsiri@umass.edu>
2018-04-28 14:40:09 -0400

    automate eval

--- experiment_lstm_lr.sh
+++ experiment_lstm_lr.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-# python train.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=0.00001
-# python train.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=0.0001
-# python train.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=0.001
-# python train.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=0.01
-python train.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=0.1
-# python train.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=1
+# python train_exp_lr.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=0.00001
+# python train_exp_lr.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=0.0001
+# python train_exp_lr.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=0.001
+# python train_exp_lr.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=0.01
+python train_exp_lr.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=0.1
+# python train_exp_lr.py --embed_size=256 --hidden_size=512 --num_layers=1 --learning_rate=1
