f1313baec65ca35d4ac6e5ab34fbe4a1bababf33
nsimsiri <nsimsiri@umass.edu>
2018-04-28 18:41:00 -0400

    training with GRU

--- experiment.sh
+++ experiment.sh
@@ -11,7 +11,7 @@ python train_gru.py--embed_size=128 --hidden_size=512 --num_layers=1
 
 python train_gru.py --embed_size=256 --hidden_size=128 --num_layers=1
 python train_gru.py --embed_size=256 --hidden_size=256 --num_layers=1
-pythontrain_gru.py --embed_size=256 --hidden_size=512 --num_layers=1
+python train_gru.py --embed_size=256 --hidden_size=512 --num_layers=1
 
 
 
