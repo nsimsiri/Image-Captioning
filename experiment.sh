76aae1ca7e690551eccc22bce9a987c884ef9a8e
nsimsiri <nsimsiri@umass.edu>
2018-04-27 22:59:42 -0400

    hmm

--- experiment.sh
+++ experiment.sh
@@ -1,24 +1,28 @@
 #! /bin/bash
-python train.py --embed_size=64 --hidden_size=128 --num_layers=1
-python train.py --embed_size=64 --hidden_size=256 --num_layers=1
-python train.py --embed_size=64 --hidden_size=512 --num_layers=1
+# python train.py --embed_size=64 --hidden_size=128 --num_layers=1
+# python train.py --embed_size=64 --hidden_size=256 --num_layers=1
+# python train.py --embed_size=64 --hidden_size=512 --num_layers=1
+#
+# python train.py --embed_size=128 --hidden_size=128 --num_layers=1
+# python train.py --embed_size=128 --hidden_size=256 --num_layers=1
+# python train.py --embed_size=128 --hidden_size=512 --num_layers=1
+#
+# python train.py --embed_size=256 --hidden_size=128 --num_layers=1
+# python train.py --embed_size=256 --hidden_size=256 --num_layers=1
+# python train.py --embed_size=256 --hidden_size=512 --num_layers=1
+#
+# python train.py --embed_size=64 --hidden_size=128 --num_layers=2
+# python train.py --embed_size=64 --hidden_size=256 --num_layers=2
+# python train.py --embed_size=64 --hidden_size=512 --num_layers=2
+#
+# python train.py --embed_size=128 --hidden_size=128 --num_layers=2
+# python train.py --embed_size=128 --hidden_size=256 --num_layers=2
+# python train.py --embed_size=128 --hidden_size=512 --num_layers=2
+#
+# python train.py --embed_size=256 --hidden_size=128 --num_layers=2
+# python train.py --embed_size=256 --hidden_size=256 --num_layers=2
+# python train.py --embed_size=256 --hidden_size=512 --num_layers=2
 
-python train.py --embed_size=128 --hidden_size=128 --num_layers=1
-python train.py --embed_size=128 --hidden_size=256 --num_layers=1
-python train.py --embed_size=128 --hidden_size=512 --num_layers=1
-
-python train.py --embed_size=256 --hidden_size=128 --num_layers=1
-python train.py --embed_size=256 --hidden_size=256 --num_layers=1
-python train.py --embed_size=256 --hidden_size=512 --num_layers=1
-
-python train.py --embed_size=64 --hidden_size=128 --num_layers=2
-python train.py --embed_size=64 --hidden_size=256 --num_layers=2
-python train.py --embed_size=64 --hidden_size=512 --num_layers=2
-
-python train.py --embed_size=128 --hidden_size=128 --num_layers=2
-python train.py --embed_size=128 --hidden_size=256 --num_layers=2
-python train.py --embed_size=128 --hidden_size=512 --num_layers=2
-
-python train.py --embed_size=256 --hidden_size=128 --num_layers=2
-python train.py --embed_size=256 --hidden_size=256 --num_layers=2
-python train.py --embed_size=256 --hidden_size=512 --num_layers=2
+python train.py --embed_size=16 --hidden_size=8 --num_layers=1
+python train.py --embed_size=16 --hidden_size=16 --num_layers=1
+python train.py --embed_size=16 --hidden_size=32 --num_layers=1
