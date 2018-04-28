b81e8c9cd018e9215034d13f99109365e2c75e48
nsimsiri <nsimsiri@umass.edu>
2018-04-27 20:49:03 -0400

    fixed repeated features

--- att_model.py
+++ att_model.py
@@ -69,7 +69,7 @@ class DecoderRNN(nn.Module):
         """Decode image feature vectors and generates captions."""
         N, L = captions.shape;
         embeddings = self.embed(captions)
-        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        # embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
         next_c = Variable(torch.zeros(N, self.hidden_size))
         # next_h = Variable(torch.zeros(N, self.hidden_size))
         next_h = self.affine_lstm_init(features);
