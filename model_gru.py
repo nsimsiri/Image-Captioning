8a24485b6a59d257b26c58b7af2500dff6992efb
nsimsiri <nsimsiri@umass.edu>
2018-04-29 10:14:35 -0400

    generating captions gru

--- model_gru.py
+++ model_gru.py
@@ -59,7 +59,7 @@ class DecoderRNN(nn.Module):
         sampled_ids = []
         inputs = features.unsqueeze(1)
         for i in range(20):                                      # maximum sampling length
-            hiddens, states = self.lstm(inputs, states)          # (batch_size, 1, hidden_size),
+            hiddens, states = self.gru(inputs, states)          # (batch_size, 1, hidden_size),
             outputs = self.linear(hiddens.squeeze(1))            # (batch_size, vocab_size)
             predicted = outputs.max(1)[1]
             sampled_ids.append(predicted)
